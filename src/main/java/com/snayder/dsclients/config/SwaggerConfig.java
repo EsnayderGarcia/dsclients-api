084f0fc6725c8e0cee5fd6e89b96aa0d2c7a5f1a
EsnayderGarcia <scriptk.18@gmail.com>
2022-10-08 19:40:56 -0300

    Documentação com Swagger

--- src/main/java/com/snayder/dsclients/config/SwaggerConfig.java
+++ src/main/java/com/snayder/dsclients/config/SwaggerConfig.java
@@ -2,11 +2,11 @@ package com.snayder.dsclients.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,19 +15,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class SwaggerConfig {
 
-    private Contact contact() {
-        return new Contact("Esnayder Garcia",
-                "https://github.com/EsnayderGarcia/dsclients-api",
-                "esnayd.25@gmail.com");
-    }
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("DSCLIENTS API")
                 .description("Aplicação para simular um crud de " +
                              "clientes de um sistema online")
                 .version("1.0")
-                .contact(contact())
                 .build();
     }
 
--- src/main/java/com/snayder/dsclients/resources/ClientResource.java
+++ src/main/java/com/snayder/dsclients/resources/ClientResource.java
@@ -3,6 +3,8 @@ package com.snayder.dsclients.resources;
 import com.snayder.dsclients.dtos.ClientDTO;
 import com.snayder.dsclients.services.ClientService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,20 +25,20 @@ public class ClientResource {
     @GetMapping
     @ApiOperation("Busca paginada de clientes")
     public ResponseEntity<Page<ClientDTO>> findAll(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size,
-            @RequestParam(defaultValue = "DESC") String direction,
-            @RequestParam(defaultValue = "income") String sort
-    ) {
+	        @RequestParam(defaultValue = "0")  @ApiParam(value = "Número da Página") int page,
+	        @RequestParam(defaultValue = "10") @ApiParam(value = "Clientes por Página") int size,
+	        @RequestParam(defaultValue = "DESC") @ApiParam(value = "Tipo da Ordenação") String direction,
+	        @RequestParam(defaultValue = "income") @ApiParam(value = "Informe por qual dado os clientes serão ordenados") String sort) {
        PageRequest pageRequest = PageRequest.of(page, size, valueOf(direction), sort);
-
-        Page<ClientDTO> dtos = this.clientService.findAll(pageRequest);
-        return ResponseEntity.ok(dtos);
+       Page<ClientDTO> dtos = this.clientService.findAll(pageRequest);
+        
+       return ResponseEntity.ok(dtos);
     }
 
     @GetMapping("/{idClient}")
     @ApiOperation("Busca de um cliente pelo id")
-    public ResponseEntity<ClientDTO> findById(@PathVariable Long idClient) {
+    public ResponseEntity<ClientDTO> findById(
+    		@PathVariable @ApiParam(value = "Id do Cliente", example = "1") Long idClient) {
         ClientDTO dto = this.clientService.findById(idClient);
         return ResponseEntity.ok(dto);
     }
@@ -47,22 +49,25 @@ public class ClientResource {
         dto = this.clientService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(dto.getId()).toUri();
+                .buildAndExpand(dto.getId())
+                .toUri();
 
         return ResponseEntity.created(uri).body(dto);
     }
 
     @PutMapping("/{idClient}")
     @ApiOperation("Atualização de um cliente pelo id")
-    public ResponseEntity<ClientDTO> update(@RequestBody ClientDTO dto,
-                                            @PathVariable Long idClient) {
-        dto = this.clientService.update(idClient, dto);
+    public ResponseEntity<ClientDTO> update(
+	    	@RequestBody ClientDTO dto,
+	    	@PathVariable @ApiParam(value = "Id do Cliente", example = "1") Long idClient) {
+    	dto = this.clientService.update(idClient, dto);
         return ResponseEntity.ok(dto);
     }
 
     @DeleteMapping("/{idClient}")
     @ApiOperation("Deleção de um cliente pelo id")
-    public ResponseEntity<Void> delete(@PathVariable Long idClient) {
+    public ResponseEntity<Void> delete(
+    		@PathVariable @ApiParam(value = "Id do Cliente", example = "1") Long idClient) {
         this.clientService.delete(idClient);
         return ResponseEntity.noContent().build();
     }
