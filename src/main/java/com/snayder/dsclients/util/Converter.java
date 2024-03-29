91e16e562458dd268ae71f48475188a0f55be88d
EsnayderGarcia <scriptk.18@gmail.com>
2024-03-29 10:39:56 -0300

    feat: Validação para quando data estiver nula

--- src/main/java/com/snayder/dsclients/util/Converter.java
+++ src/main/java/com/snayder/dsclients/util/Converter.java
@@ -2,9 +2,13 @@ package com.snayder.dsclients.util;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Converter {
     public static String localdateToString(LocalDate localDate) {
+        if (Objects.isNull(localDate))
+            return "";
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return localDate.format(formatter);
     }
